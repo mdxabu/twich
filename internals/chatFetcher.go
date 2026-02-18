9d9b344d41f241797f37fced6c765b35ccd43e12
Abu <abdullahfakrudeen2020@gmail.com>
2026-02-18 15:22:16 +0530

    Gave a usercolor map to store the assigned color

--- internals/chatFetcher.go
+++ internals/chatFetcher.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-
 	"github.com/fatih/color"
 	"github.com/gempir/go-twitch-irc/v4"
 )
 
 	var MessageColor = color.New(color.FgHiWhite)
 		
-	var userColor = []color.Attribute{
+	var userColors = []color.Attribute{
 		color.FgRed,
-		// color.FgBlue,
 		color.FgCyan,
 		color.FgGreen,
 		color.FgMagenta,
-		// color.FgHiBlue,
 		color.FgHiCyan,
 		color.FgHiGreen,
 		color.FgHiRed,
 		color.FgHiMagenta,
 	}
+	
+	// Map to store color for per user
+	var userColorMap = make(map[string]*color.Color)
 		
 		
 
@@ -40,14 +40,21 @@ func FetchChat(username string) {
 			message.Time.Local().Format("15:04:05"),
 		)
 		
-		// randcom color for the usernames/users
-		randomColor := userColor[rand.Intn(len(userColor))]
-		userColor := color.New(randomColor)
+		displayName := message.User.DisplayName
 		
-		user := userColor.Sprintf(message.User.DisplayName)
+		userColor, exists := userColorMap[displayName]
+		
+		if !exists{
+			randomAttr := userColors[rand.Intn(len(userColors))]
+			userColor = color.New(randomAttr)
+			userColorMap[displayName] = userColor
+		}
+		
+		user := userColor.Sprintf(displayName)
 		
 		// message color
 		msg := MessageColor.Sprintf(message.Message)
+		
 		
 		fmt.Printf("%s [%s]: %s\n",timestamp,user,msg)
 		
